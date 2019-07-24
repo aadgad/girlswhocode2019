bebd8d0813765b3716606424a32cfcf6f52b8b8a
Aadya Gadkari <aadyagadkari@gmail.com>
2019-07-24 10:24:14 -0400

    filters

--- filters.py
+++ filters.py
@@ -39,13 +39,13 @@ def save_img(im, fileName):
 
 def obamicon(flowerimage):
     flowerimage.getdata(band = None)
-    a = list(image.getdata())
+    og = list(image.getdata())
     darkBlue = (0,51,76)
     red = (217, 26, 33)
     lightBlue = (112, 150, 158)
     yellow = (252, 227, 166)
     newimg = []
-    for x in a:
+    for x in og:
         intensity = x[0] + x[1] + x[2]
 
         if intensity < 182:
@@ -59,15 +59,44 @@ def obamicon(flowerimage):
         elif intensity >= 546:
             newimg.append(yellow)
 
-    #newflower = Image.new("RGB," (250,250), color = 0)
+    newflower = Image.new("RGB" ,(250,250))
     newflower = flowerimage.putdata(newimg)
-    def save_img(flowerimage, newflower):
-        flowerimage.save(newflower)
-    save_img(flowerimage, "newImage.jpg")
-    newpic = load_img("newImage.jpg")
-    show_img(newpic)
-    #newimg.show(title = None)
-    #return newflower
+    return newflower
+
+
+def aadyainvfilter(flowerimage):
+    flowerimage.getdata(band = None)
+    f = list(image.getdata())
+    invlist = []
+    for colors in f:
+        x1= 255- colors[0]
+        x2= 255- colors[1]
+        x3= 255- colors[2]
+        invlist.append((x1, x2, x3))
+
+    nflower = flowerimage.putdata(invlist)
+    newflower = Image.new("RGB" ,(250,250))
+    newflower = image.putdata(invlist)
+    return newflower
+
+
+def halfilter(flowerimage):
+    flowerimage.getdata(band=None)
+
+    d = list(image.getdata())
+    halflengthlist= []
+    halflength = int((len(d))/2)
+
+    for colors in range(halflength):
+        x1= 255- colors[0]
+        x2= 255- colors[1]
+        x3= 255- colors[2]
+        halflengthlist.append((x1, x2, x3))
+
+    halfflower = flowerimage.putdata(halflengthlist)
+    half2 = Image.new("RGB" ,(250,250))
+    half2 = image.putdata(halflengthlist)
+    return newflower
 
 
 
@@ -76,4 +105,9 @@ load_img("flowers.jpg")
 image = load_img("flowers.jpg")
 show_img(image)
 #save_img(image, "flowers2.jpg")
-obamicon(image)
+#obamicon(image)
+#show_img(image)
+#aadyainvfilter(image)
+#show_img(image)
+halfilter(image)
+show_img(image)
